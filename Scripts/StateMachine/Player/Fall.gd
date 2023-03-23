b5577982f22fc118d7794d360be03bf73dbb8f75
Jonathan Platner <42499453+JonathanPlatner@users.noreply.github.com>
2023-03-23 14:43:17 -0700

    add jump buffering

--- Scripts/StateMachine/Player/Fall.gd
+++ Scripts/StateMachine/Player/Fall.gd
@@ -8,16 +8,27 @@ extends State
 @export var acceleration: float = 200.0
 @export var gravity: float = 1100.0
 @export var max_fall_speed: float = 400
-@export var coyote_time: float = 0.25
+@export var coyote_time: float = 0.15
+@export var jump_buffer_time: float = 0.1
 
 @onready var _idle_state: State = get_node(idle_node)
 @onready var _move_state: State = get_node(move_node)
 @onready var _jump_state: State = get_node(jump_node)
 
+var buffering_jump: bool = false
+var jump_buffer_elapsed: float = 0.0
+
+func enter(previous_state: State):
+	super(previous_state)
+	jump_buffer_elapsed = 0.0
+
 func input(event: InputEvent) -> State:
 	if Input.is_action_just_pressed("jump"):
 		if _previous_state is Move and _state_timer <= coyote_time:
 			return _jump_state
+		else:
+			buffering_jump = true
+			jump_buffer_elapsed = 0.0
 	return null
 
 func physics_process(delta: float) -> State:
@@ -35,7 +46,14 @@ func physics_process(delta: float) -> State:
 	_entity.velocity = velocity
 	_entity.move_and_slide()
 	
+	if buffering_jump:
+		jump_buffer_elapsed += delta
+		if jump_buffer_elapsed >= jump_buffer_time:
+			buffering_jump = false
+	
 	if _entity.is_on_floor():
+		if buffering_jump:
+			return _jump_state
 		if _entity.velocity.x == 0 and target_velocity == 0:
 			return _idle_state
 		else:
